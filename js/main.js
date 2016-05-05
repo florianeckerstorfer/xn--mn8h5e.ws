194bded57999dcdc95f463e5133dc8787f6b81d2
Florian Eckerstorfer <florian@eckerstorfer.co>
2016-05-05 21:36:29 +0200

    Remove 3d touch stuff

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 (function () {
-    var authorEl = document.querySelector('.author a');
-
     function rand(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
-    }
-
-    function enterForceClick(e) {
-        authorEl.focus();
     }
 
     window.onload = function () {
@@ -14,6 +8,4 @@
             hands = ['\uD83D\uDC4B\uD83C\uDFFB', '\uD83D\uDC4B\uD83C\uDFFC', '\uD83D\uDC4B\uD83C\uDFFC', '\uD83D\uDC4B\uD83C\uDFFC', '\uD83D\uDC4B\uD83C\uDFFC'];
         wel.textContent = hands[rand(0, 5)];
     };
-
-    authorEl.addEventListener('webkitmouseforcedown', enterForceClick, false);
 })();
